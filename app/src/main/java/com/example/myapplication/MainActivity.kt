233fe57b9c0caa45817c1ed4b29b6e5b12d7586c
Appjey <loikoanton@yandex.ru>
2022-05-13 19:13:12 +0300

    alpha 0.0.1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityAuthBinding
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        binding.bLogOut.setOnClickListener{
+            auth.signOut()
+            val i = Intent(this, Auth::class.java)
+            startActivity(i)
+        }
+    }
+}
