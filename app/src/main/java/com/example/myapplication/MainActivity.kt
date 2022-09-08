c1e1e6de3d66c058a0f117a770729656a97c3b5b
Appjey <loikoanton@yandex.ru>
2022-09-08 16:46:06 +0300

    0.0.6A-Alpha

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,24 +1,41 @@
 package com.example.myapplication
 
+import android.R
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.myapplication.databinding.ActivityAuthBinding
+import android.util.Log
+import android.view.View
+import android.webkit.WebSettings
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
-import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
+
 class MainActivity : AppCompatActivity() {
+
+
     lateinit var binding: ActivityMainBinding
     lateinit var auth: FirebaseAuth
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
+        val idUrl = intent?.getStringExtra("film")?:"Error"
+        Log.e("idUrl", idUrl)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = Firebase.auth
-
+        val myWebView = WebView(this)
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.settings.domStorageEnabled = true
+        myWebView.settings.setAppCacheEnabled(true)
+        myWebView.settings.cacheMode = WebSettings.LOAD_DEFAULT
+        myWebView.settings.setSupportZoom(true)
+        myWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        myWebView.loadUrl(idUrl)
         binding.bLogOut.setOnClickListener{
             auth.signOut()
             val i = Intent(this, Auth::class.java)
--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
@@ -1,17 +1,22 @@
 package com.example.myapplication.ui.home
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import coil.load
 import coil.transform.CircleCropTransformation
+import com.example.myapplication.HelloScreen
+import com.example.myapplication.MainActivity
+import com.example.myapplication.MainActivity2
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentHomeBinding
 import com.google.firebase.database.DataSnapshot
@@ -20,6 +25,7 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import kotlin.math.log
 
 class HomeFragment : Fragment() {
 
@@ -63,6 +69,7 @@ class HomeFragment : Fragment() {
                     binding.textHome.text = pathToImage
                     binding.imageView5.load(pathToImage) {
                         Log.e("TAG", "onDataChange: $pathToImage")
+// set on-click listener for ImageView
                         crossfade(false)
                         placeholder(R.drawable.ic_launcher_background)
                         transformations(CircleCropTransformation())
@@ -73,9 +80,30 @@ class HomeFragment : Fragment() {
                     Log.w(TAG, "Error message", databaseError.toException())
                 }
             }
+        val filmListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val pathToFilm = dataSnapshot!!.getValue<String>()
+                Log.e(TAG, "film: $pathToFilm")
+
+                binding.textHome.text = pathToFilm
+                binding.imageView5.setOnClickListener {
+
+                    Log.e("TAG", "Click: $pathToFilm")
+                    val intent = Intent(context, MainActivity::class.java)
+                    intent.putExtra("film", pathToFilm.toString())
+                    startActivity(intent)
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "Error message", databaseError.toException())
+            }
+        }
 
             val image = database.getReference("1").child("jpg")
+            val film = database.getReference("1").child("film")
             image.addValueEventListener(imageListener)
+            film.addValueEventListener(filmListener)
 
         }
 
